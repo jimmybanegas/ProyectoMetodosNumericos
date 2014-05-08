c8a5b7d0e264e921afb68fbdb792d5bfd23d80a3
William <william.zx1@unitec.edu>
2014-05-08 16:48:03 -0600

    First commit

---
+++ ProyectoMetodosNumericos/main.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  5 15:03:54 2014
+
+@author: Juan Carlos Espinza, William Avila
+"""
+import sys
+from PyQt4 import QtGui
+from bisection import bis
+import visualFunctions
+from mwindow import Ui_MWindow
+
+class Main(QtGui.QMainWindow):
+        def __init__(self):
+            QtGui.QMainWindow.__init__(self)
+            self.ui = Ui_MWindow()
+            self.ui.setupUi(self)
+            self.ui.pbCalculate.clicked.connect(self.test)
+            self.ui.cboMethod.currentIndexChanged.connect(self.checkIndex)
+            self.ui.leParam1.setText("")
+            self.ui.leParam2.setText("")
+            self.ui.leParam3.setText("")
+            self.ui.leParam4.setText("")
+            self.ui.leParam5.setText("")
+            self.ui.leParam6.setText("")
+            self.ui.leParam7.setText("")
+            self.ui.leParam8.setText("")
+            self.ui.lbParam1.setText("Param1")
+            self.ui.lbParam2.setText("Param2")
+            self.ui.lbParam3.setText("Param3")
+            self.ui.lbParam4.setText("Param4")
+            self.ui.lbParam5.setText("Param5")
+            self.ui.lbParam6.setText("Param6")
+            self.ui.lbParam7.setText("Param7")
+            self.ui.lbParam8.setText("Param8")
+            self.ui.leParam1.hide()
+            self.ui.leParam2.hide()
+            self.ui.leParam3.hide()
+            self.ui.leParam4.hide()
+            self.ui.leParam5.hide()
+            self.ui.leParam6.hide()
+            self.ui.leParam7.hide()
+            self.ui.leParam8.hide()
+            self.ui.lbParam1.hide()
+            self.ui.lbParam2.hide()
+            self.ui.lbParam3.hide()
+            self.ui.lbParam4.hide()
+            self.ui.lbParam5.hide()
+            self.ui.lbParam6.hide()
+            self.ui.lbParam7.hide()
+            self.ui.lbParam8.hide()   
+            self.ui.cboMethod.clearFocus()      
+            
+            self.ui.leEquation.setText("")
+            #self.ui.lbResult.hide()
+            
+            self.ui.pbCalculate.clicked.connect(self.pushbutton_Clicked)
+            
+            
+            
+            
+        def pushbutton_Clicked(self):
+            lista=[]
+            #bis("x**2 +4*(x**2)-10",1,2,0.00005,50)
+            #test=bis("2*(x**3)-4*(x**2)+4*(x)+4",-1,-2,0.00005,50)
+            try:
+                resp=bis(str(self.ui.leEquation.text()),int(str(self.ui.leParam1.text())),int(str(self.ui.leParam2.text())),float(str(self.ui.leParam3.text())),int(str(self.ui.leParam4.text())),lista)
+                for n in lista:
+                        self.ui.teSteps.append(n)
+            
+                self.ui.lbResult.setText(str(resp))
+            except:
+                self.ui.teSteps.clear()
+                self.ui.teSteps.setText("Ingrese los datos corectamente")
+              
+               
+            
+            
+            
+        def checkIndex(self):
+            index = self.ui.cboMethod.currentIndex()
+            def methodBisection():
+                self.ui.lbParam1.setText("Limite inferior A")
+                self.ui.lbParam2.setText("Limite superior B")
+                self.ui.lbParam3.setText("Tolerancia")
+                self.ui.lbParam4.setText("No. Iteraciones")
+                self.ui.leParam1.show()
+                self.ui.leParam2.show()
+                self.ui.leParam3.show()
+                self.ui.leParam4.show()
+                self.ui.lbParam1.show()
+                self.ui.lbParam2.show()
+                self.ui.lbParam3.show()
+                self.ui.lbParam4.show()
+                
+            def methodPuntoFijo():
+                self.ui.lbParam1.setText("Ecuacion Secundaria")
+                self.ui.lbParam2.setText("Aproximacion Inicial")
+                self.ui.lbParam3.setText("Tolerancia")
+                self.ui.lbParam4.setText("No. Iteraciones")
+                self.ui.leParam1.show()
+                self.ui.leParam2.show()
+                self.ui.leParam3.show()
+                self.ui.leParam4.show()
+                self.ui.lbParam1.show()
+                self.ui.lbParam2.show()
+                self.ui.lbParam3.show()
+                self.ui.lbParam4.show()
+                
+            def methodNewton():
+                self.ui.lbParam1.setText("Aproximacion Inicial")
+                self.ui.lbParam2.setText("Tolerancia")
+                self.ui.lbParam3.setText("No. Iteraciones")
+                self.ui.leParam1.show()
+                self.ui.leParam2.show()
+                self.ui.leParam3.show()
+                self.ui.lbParam1.show()
+                self.ui.lbParam2.show()
+                self.ui.lbParam3.show()
+                
+            def methodSecanteAndPosicionFalsa():
+                self.ui.lbParam1.setText("Aproximacion Inicial P0")
+                self.ui.lbParam2.setText("Aproximacion Inicial P1")
+                self.ui.lbParam3.setText("Tolerancia")
+                self.ui.lbParam4.setText("No. Iteraciones")
+                self.ui.leParam1.show()
+                self.ui.leParam2.show()
+                self.ui.leParam3.show()
+                self.ui.leParam4.show()
+                self.ui.lbParam1.show()
+                self.ui.lbParam2.show()
+                self.ui.lbParam3.show()
+                self.ui.lbParam4.show()
+                
+            def methodMuller():
+                self.ui.lbParam1.setText("Aproximacion Inicial 1")
+                self.ui.lbParam2.setText("Aproximacion Inicial 2")
+                self.ui.lbParam3.setText("Aproximacion Inicial 3")
+                self.ui.lbParam4.setText("Tolerancia")
+                self.ui.lbParam5.setText("No. Iteraciones")
+                self.ui.leParam1.show()
+                self.ui.leParam2.show()
+                self.ui.leParam3.show()
+                self.ui.leParam4.show()
+                self.ui.leParam5.show()
+                self.ui.lbParam1.show()
+                self.ui.lbParam2.show()
+                self.ui.lbParam3.show()
+                self.ui.lbParam4.show()
+                self.ui.lbParam5.show()
+                
+                
+            options = {
+                0 : methodBisection,
+                1 : methodPuntoFijo,
+                2 : methodNewton,
+                3 : methodSecanteAndPosicionFalsa,
+                4 : methodSecanteAndPosicionFalsa,
+                5 : methodMuller
+            }
+            options[index]()
+          
+            
+            # def resetAll(self):
+            
+            
+        def cleanHistory(self):
+            self.ui.teSteps("")
+        
+
+            
+            
+        def test(self):
+            
+            '''
+            resp=bis(str(self.ui.lineEdit.text()),int(str(self.ui.lineEdit_2.text())),int(str(self.ui.lineEdit_3.text())),float(str(self.ui.lineEdit_4.text())),int(str(self.ui.lineEdit_5.text())),lista)
+            for n in lista:
+                self.ui.textEditMessage.append(n)
+            self.ui.lineEdit_6Repuesta.setText(str(resp))
+            '''
+            lista = []
+            res = bis("x**2 +4*(x**2)-10",1,2,0.00005,50, lista)
+            
+            print(res)
+        
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    mwindow = Main()
+    mwindow.show()
+    sys.exit(app.exec_())
---
+++ ProyectoMetodosNumericos/mwindow.py
@@ -0,0 +1,298 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mwindow.ui'
+#
+# Created: Tue May  6 23:06:18 2014
+#      by: PyQt4 UI code generator 4.9.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MWindow(object):
+    def setupUi(self, MWindow):
+        MWindow.setObjectName(_fromUtf8("MWindow"))
+        MWindow.resize(710, 508)
+        self.centralwidget = QtGui.QWidget(MWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(480, 40, 61, 21))
+        self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.cboMethod = QtGui.QComboBox(self.centralwidget)
+        self.cboMethod.setGeometry(QtCore.QRect(550, 40, 115, 20))
+        self.cboMethod.setObjectName(_fromUtf8("cboMethod"))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(15, 40, 61, 21))
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.leEquation = QtGui.QLineEdit(self.centralwidget)
+        self.leEquation.setGeometry(QtCore.QRect(80, 40, 259, 20))
+        self.leEquation.setObjectName(_fromUtf8("leEquation"))
+        self.groupParameters = QtGui.QGroupBox(self.centralwidget)
+        self.groupParameters.setGeometry(QtCore.QRect(10, 70, 681, 121))
+        self.groupParameters.setObjectName(_fromUtf8("groupParameters"))
+        self.leParam4 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam4.setGeometry(QtCore.QRect(350, 20, 81, 20))
+        self.leParam4.setObjectName(_fromUtf8("leParam4"))
+        self.leParam6 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam6.setGeometry(QtCore.QRect(350, 80, 81, 20))
+        self.leParam6.setObjectName(_fromUtf8("leParam6"))
+        self.lbParam3 = QtGui.QLabel(self.groupParameters)
+        self.lbParam3.setGeometry(QtCore.QRect(10, 80, 91, 21))
+        self.lbParam3.setObjectName(_fromUtf8("lbParam3"))
+        self.lbParam7 = QtGui.QLabel(self.groupParameters)
+        self.lbParam7.setGeometry(QtCore.QRect(490, 20, 81, 21))
+        self.lbParam7.setObjectName(_fromUtf8("lbParam7"))
+        self.lbParam6 = QtGui.QLabel(self.groupParameters)
+        self.lbParam6.setGeometry(QtCore.QRect(250, 80, 91, 21))
+        self.lbParam6.setObjectName(_fromUtf8("lbParam6"))
+        self.leParam3 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam3.setGeometry(QtCore.QRect(110, 80, 81, 20))
+        self.leParam3.setObjectName(_fromUtf8("leParam3"))
+        self.lbParam8 = QtGui.QLabel(self.groupParameters)
+        self.lbParam8.setGeometry(QtCore.QRect(490, 50, 81, 21))
+        self.lbParam8.setObjectName(_fromUtf8("lbParam8"))
+        self.lbParam4 = QtGui.QLabel(self.groupParameters)
+        self.lbParam4.setGeometry(QtCore.QRect(250, 20, 91, 21))
+        self.lbParam4.setObjectName(_fromUtf8("lbParam4"))
+        self.leParam8 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam8.setGeometry(QtCore.QRect(580, 50, 81, 20))
+        self.leParam8.setObjectName(_fromUtf8("leParam8"))
+        self.leParam2 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam2.setGeometry(QtCore.QRect(110, 50, 81, 20))
+        self.leParam2.setObjectName(_fromUtf8("leParam2"))
+        self.leParam5 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam5.setGeometry(QtCore.QRect(350, 50, 81, 20))
+        self.leParam5.setObjectName(_fromUtf8("leParam5"))
+        self.leParam7 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam7.setGeometry(QtCore.QRect(580, 20, 81, 20))
+        self.leParam7.setObjectName(_fromUtf8("leParam7"))
+        self.lbParam2 = QtGui.QLabel(self.groupParameters)
+        self.lbParam2.setGeometry(QtCore.QRect(10, 50, 91, 21))
+        self.lbParam2.setObjectName(_fromUtf8("lbParam2"))
+        self.leParam1 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam1.setGeometry(QtCore.QRect(110, 20, 81, 20))
+        self.leParam1.setObjectName(_fromUtf8("leParam1"))
+        self.lbParam1 = QtGui.QLabel(self.groupParameters)
+        self.lbParam1.setGeometry(QtCore.QRect(10, 20, 91, 21))
+        self.lbParam1.setObjectName(_fromUtf8("lbParam1"))
+        self.lbParam5 = QtGui.QLabel(self.groupParameters)
+        self.lbParam5.setGeometry(QtCore.QRect(250, 50, 91, 21))
+        self.lbParam5.setObjectName(_fromUtf8("lbParam5"))
+        self.pbCalculate = QtGui.QPushButton(self.groupParameters)
+        self.pbCalculate.setGeometry(QtCore.QRect(524, 90, 141, 23))
+        self.pbCalculate.setObjectName(_fromUtf8("pbCalculate"))
+        self.teSteps = QtGui.QTextEdit(self.centralwidget)
+        self.teSteps.setGeometry(QtCore.QRect(10, 200, 681, 231))
+        self.teSteps.setObjectName(_fromUtf8("teSteps"))
+        self.lbResult = QtGui.QLabel(self.centralwidget)
+        self.lbResult.setGeometry(QtCore.QRect(545, 440, 121, 21))
+        self.lbResult.setObjectName(_fromUtf8("lbResult"))
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(470, 440, 61, 21))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        MWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 710, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MWindow)
+        QtCore.QMetaObject.connectSlotsByName(MWindow)
+
+    def retranslateUi(self, MWindow):
+        MWindow.setWindowTitle(_translate("MWindow", "Resolucion de Ecuaciones", None))
+        self.label_2.setText(_translate("MWindow", "Metodo:", None))
+        self.cboMethod.setItemText(0, _translate("MWindow", "Biseccion", None))
+        self.cboMethod.setItemText(1, _translate("MWindow", "Iteracion de punto fijo", None))
+        self.cboMethod.setItemText(2, _translate("MWindow", "Newton", None))
+        self.cboMethod.setItemText(3, _translate("MWindow", "Secante", None))
+        self.cboMethod.setItemText(4, _translate("MWindow", "Posicion Falsa", None))
+        self.cboMethod.setItemText(5, _translate("MWindow", "Muller", None))
+        self.label.setText(_translate("MWindow", "Ecuacion:", None))
+        self.groupParameters.setTitle(_translate("MWindow", "Parametros", None))
+        self.lbParam3.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam7.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam6.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam8.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam4.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam2.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam1.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam5.setText(_translate("MWindow", "Param1:", None))
+        self.pbCalculate.setText(_translate("MWindow", "Calcular", None))
+        self.lbResult.setText(_translate("MWindow", "TextLabel", None))
+        self.label_3.setText(_translate("MWindow", "Resultado:", None))
+
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mwindow.ui'
+#
+# Created: Thu May 08 15:21:50 2014
+#      by: PyQt4 UI code generator 4.9.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MWindow(object):
+    def setupUi(self, MWindow):
+        MWindow.setObjectName(_fromUtf8("MWindow"))
+        MWindow.resize(763, 504)
+        self.centralwidget = QtGui.QWidget(MWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(480, 40, 61, 21))
+        self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.cboMethod = QtGui.QComboBox(self.centralwidget)
+        self.cboMethod.setGeometry(QtCore.QRect(550, 40, 115, 20))
+        self.cboMethod.setObjectName(_fromUtf8("cboMethod"))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.cboMethod.addItem(_fromUtf8(""))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(15, 40, 61, 21))
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.leEquation = QtGui.QLineEdit(self.centralwidget)
+        self.leEquation.setGeometry(QtCore.QRect(80, 40, 259, 20))
+        self.leEquation.setObjectName(_fromUtf8("leEquation"))
+        self.groupParameters = QtGui.QGroupBox(self.centralwidget)
+        self.groupParameters.setGeometry(QtCore.QRect(10, 70, 721, 121))
+        self.groupParameters.setObjectName(_fromUtf8("groupParameters"))
+        self.leParam4 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam4.setGeometry(QtCore.QRect(380, 20, 81, 20))
+        self.leParam4.setObjectName(_fromUtf8("leParam4"))
+        self.leParam6 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam6.setGeometry(QtCore.QRect(380, 80, 81, 20))
+        self.leParam6.setObjectName(_fromUtf8("leParam6"))
+        self.lbParam3 = QtGui.QLabel(self.groupParameters)
+        self.lbParam3.setGeometry(QtCore.QRect(10, 80, 131, 21))
+        self.lbParam3.setObjectName(_fromUtf8("lbParam3"))
+        self.lbParam7 = QtGui.QLabel(self.groupParameters)
+        self.lbParam7.setGeometry(QtCore.QRect(490, 20, 81, 21))
+        self.lbParam7.setObjectName(_fromUtf8("lbParam7"))
+        self.lbParam6 = QtGui.QLabel(self.groupParameters)
+        self.lbParam6.setGeometry(QtCore.QRect(250, 80, 91, 21))
+        self.lbParam6.setObjectName(_fromUtf8("lbParam6"))
+        self.leParam3 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam3.setGeometry(QtCore.QRect(140, 80, 81, 20))
+        self.leParam3.setObjectName(_fromUtf8("leParam3"))
+        self.lbParam8 = QtGui.QLabel(self.groupParameters)
+        self.lbParam8.setGeometry(QtCore.QRect(490, 50, 81, 21))
+        self.lbParam8.setObjectName(_fromUtf8("lbParam8"))
+        self.lbParam4 = QtGui.QLabel(self.groupParameters)
+        self.lbParam4.setGeometry(QtCore.QRect(250, 20, 121, 21))
+        self.lbParam4.setObjectName(_fromUtf8("lbParam4"))
+        self.leParam8 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam8.setGeometry(QtCore.QRect(580, 50, 81, 20))
+        self.leParam8.setObjectName(_fromUtf8("leParam8"))
+        self.leParam2 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam2.setGeometry(QtCore.QRect(140, 50, 81, 20))
+        self.leParam2.setObjectName(_fromUtf8("leParam2"))
+        self.leParam5 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam5.setGeometry(QtCore.QRect(380, 50, 81, 20))
+        self.leParam5.setObjectName(_fromUtf8("leParam5"))
+        self.leParam7 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam7.setGeometry(QtCore.QRect(580, 20, 81, 20))
+        self.leParam7.setObjectName(_fromUtf8("leParam7"))
+        self.lbParam2 = QtGui.QLabel(self.groupParameters)
+        self.lbParam2.setGeometry(QtCore.QRect(10, 50, 131, 21))
+        self.lbParam2.setObjectName(_fromUtf8("lbParam2"))
+        self.leParam1 = QtGui.QLineEdit(self.groupParameters)
+        self.leParam1.setGeometry(QtCore.QRect(140, 20, 81, 20))
+        self.leParam1.setObjectName(_fromUtf8("leParam1"))
+        self.lbParam1 = QtGui.QLabel(self.groupParameters)
+        self.lbParam1.setGeometry(QtCore.QRect(10, 20, 131, 21))
+        self.lbParam1.setObjectName(_fromUtf8("lbParam1"))
+        self.lbParam5 = QtGui.QLabel(self.groupParameters)
+        self.lbParam5.setGeometry(QtCore.QRect(250, 50, 121, 21))
+        self.lbParam5.setObjectName(_fromUtf8("lbParam5"))
+        self.pbCalculate = QtGui.QPushButton(self.groupParameters)
+        self.pbCalculate.setGeometry(QtCore.QRect(524, 90, 141, 23))
+        self.pbCalculate.setObjectName(_fromUtf8("pbCalculate"))
+        self.teSteps = QtGui.QTextEdit(self.centralwidget)
+        self.teSteps.setGeometry(QtCore.QRect(10, 200, 721, 231))
+        self.teSteps.setObjectName(_fromUtf8("teSteps"))
+        self.lbResult = QtGui.QLabel(self.centralwidget)
+        self.lbResult.setGeometry(QtCore.QRect(545, 440, 121, 21))
+        self.lbResult.setText(_fromUtf8(""))
+        self.lbResult.setObjectName(_fromUtf8("lbResult"))
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(470, 440, 61, 21))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        MWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 763, 18))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MWindow)
+        QtCore.QMetaObject.connectSlotsByName(MWindow)
+
+    def retranslateUi(self, MWindow):
+        MWindow.setWindowTitle(_translate("MWindow", "Resolucion de Ecuaciones", None))
+        self.label_2.setText(_translate("MWindow", "Metodo:", None))
+        self.cboMethod.setItemText(0, _translate("MWindow", "Biseccion", None))
+        self.cboMethod.setItemText(1, _translate("MWindow", "Iteracion de punto fijo", None))
+        self.cboMethod.setItemText(2, _translate("MWindow", "Newton", None))
+        self.cboMethod.setItemText(3, _translate("MWindow", "Secante", None))
+        self.cboMethod.setItemText(4, _translate("MWindow", "Posicion Falsa", None))
+        self.cboMethod.setItemText(5, _translate("MWindow", "Muller", None))
+        self.label.setText(_translate("MWindow", "Ecuacion:", None))
+        self.groupParameters.setTitle(_translate("MWindow", "Parametros", None))
+        self.lbParam3.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam7.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam6.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam8.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam4.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam2.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam1.setText(_translate("MWindow", "Param1:", None))
+        self.lbParam5.setText(_translate("MWindow", "Param1:", None))
+        self.pbCalculate.setText(_translate("MWindow", "Calcular", None))
+        self.label_3.setText(_translate("MWindow", "Resultado:", None))
+

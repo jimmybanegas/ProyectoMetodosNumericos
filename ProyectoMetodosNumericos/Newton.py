a3e303b53b157611302ccf09ca42bcfde952bfa0
William <william.zx1@unitec.edu>
2014-05-10 12:27:56 -0600

    Bisection and Newton working!!

---
+++ ProyectoMetodosNumericos/Newton.py
@@ -0,0 +1,82 @@
+#coding=utf-8
+from math import *
+
+import parser
+
+from sympy import *
+def Newt(ecuacion,a,tol,n,lista):
+    x = Symbol ('x')    
+        
+        
+    lista.append ('METODO DE NEWTON')    
+    lista.append ('Para obtener una solucion a f(x) = 0 dada la funcion diferenciable f y una aproximacion inicial p_0.')    
+    lista.append (' ')    
+        
+    """ecuacion = raw_input('Introduzca la ecuacion a resolver en forma de funcion: f(x)= ')    
+    a = float(input('Introduzca aproximacion inicial (p_0): '))    
+    tol = float(input('Introduzca tolerancia: '))    
+    n = int(raw_input('Introduzca el numero de iteraciones que desea realizar (i): '))    
+    lista.append (' ')"""
+
+    primera_derivada = str(diff(ecuacion, x))    
+        
+    def f(x):    
+        return eval(ecuacion)    
+    def d(x):    
+        return eval(primera_derivada)    
+        
+    i = 1    
+    P_0 = a    
+        
+    lista.append ("")    
+    lista.append ("Paso #1")    
+    lista.append ("Tome i = " + str(i))    
+        
+    lista.append ("")    
+    lista.append ("Paso #2")    
+    lista.append ("Mientras i sea menor o igual al numero de iteraciones")    
+    lista.append ("Hacer los pasos 3, 4, 5 y 6")    
+    lista.append ("")    
+        
+    while i <= n:    
+        lista.append ("------------------ i = " + str(i) + " ------------------")    
+                
+        lista.append ("Paso #3")    
+        P = P_0 - (f(P_0)/d(P_0))                lista.append (str(P)+"")
+        P = float(P)    
+            
+           
+            
+        lista.append ("p = "+str(P_0)+" - ("+str(f(P_0))+"/"+str(d(P_0))+")")    
+        lista.append ("p = "+str(P))    
+        lista.append ("")    
+            
+        lista.append ("Paso #4")    
+        t = abs(P - P_0)    
+        lista.append ("|"+str(P)+" - "+str(P_0)+"| < "+str(tol))    
+            
+        if t < tol:    
+            s = "VERDADERO"    
+        else:    
+            s = "FALSO"    
+            
+        lista.append (str(t)+" < "+str(tol)+" ? : "+s)    
+        lista.append ("")    
+        if t < tol:
+            break    
+        
+        lista.append ("Paso #5")    
+        i += 1    
+        lista.append ("i = " + str(i))    
+        lista.append ("")    
+        
+        lista.append ("Paso #6")    
+        P_0 = P    
+        lista.append ("p_0 = " + str(P_0))    
+        lista.append ("")    
+        
+    if s == "VERDADERO":            return P    
+        
+    else:        
+        lista.append ("El método fracasó luego de "+str(n)+" iteraciones")        return 0
+lista=[]"""Newt("cos(x)-x",1,0.00005,50,lista)for c in lista:    print(c)"""
--- ProyectoMetodosNumericos/main.py
+++ ProyectoMetodosNumericos/main.py
@@ -7,79 +7,62 @@ Created on Mon May  5 15:03:54 2014
 import sys
 from PyQt4 import QtGui
 from bisection import bis
-import visualFunctions
+from Newton import Newt
+from visualFunctions import resetAll, hideAll
 from mwindow import Ui_MWindow
 
 class Main(QtGui.QMainWindow):
+        #Init
         def __init__(self):
             QtGui.QMainWindow.__init__(self)
             self.ui = Ui_MWindow()
             self.ui.setupUi(self)
-            self.ui.pbCalculate.clicked.connect(self.test)
-            self.ui.cboMethod.currentIndexChanged.connect(self.checkIndex)
-            self.ui.leParam1.setText("")
-            self.ui.leParam2.setText("")
-            self.ui.leParam3.setText("")
-            self.ui.leParam4.setText("")
-            self.ui.leParam5.setText("")
-            self.ui.leParam6.setText("")
-            self.ui.leParam7.setText("")
-            self.ui.leParam8.setText("")
-            self.ui.lbParam1.setText("Param1")
-            self.ui.lbParam2.setText("Param2")
-            self.ui.lbParam3.setText("Param3")
-            self.ui.lbParam4.setText("Param4")
-            self.ui.lbParam5.setText("Param5")
-            self.ui.lbParam6.setText("Param6")
-            self.ui.lbParam7.setText("Param7")
-            self.ui.lbParam8.setText("Param8")
-            self.ui.leParam1.hide()
-            self.ui.leParam2.hide()
-            self.ui.leParam3.hide()
-            self.ui.leParam4.hide()
-            self.ui.leParam5.hide()
-            self.ui.leParam6.hide()
-            self.ui.leParam7.hide()
-            self.ui.leParam8.hide()
-            self.ui.lbParam1.hide()
-            self.ui.lbParam2.hide()
-            self.ui.lbParam3.hide()
-            self.ui.lbParam4.hide()
-            self.ui.lbParam5.hide()
-            self.ui.lbParam6.hide()
-            self.ui.lbParam7.hide()
-            self.ui.lbParam8.hide()   
-            self.ui.cboMethod.clearFocus()      
-            
+            self.ui.cboMethod.currentIndexChanged.connect(self.checkIndex) 
             self.ui.leEquation.setText("")
-            #self.ui.lbResult.hide()
-            
-            self.ui.pbCalculate.clicked.connect(self.pushbutton_Clicked)
-            
+            self.checkIndex()
+           
             
             
-            
-        def pushbutton_Clicked(self):
+        def pushbutton_ClickedBiseccion(self):
             lista=[]
             #bis("x**2 +4*(x**2)-10",1,2,0.00005,50)
             #test=bis("2*(x**3)-4*(x**2)+4*(x)+4",-1,-2,0.00005,50)
             try:
+               
                 resp=bis(str(self.ui.leEquation.text()),int(str(self.ui.leParam1.text())),int(str(self.ui.leParam2.text())),float(str(self.ui.leParam3.text())),int(str(self.ui.leParam4.text())),lista)
                 for n in lista:
-                        self.ui.teSteps.append(n)
+                    self.ui.teSteps.append(n)
             
                 self.ui.lbResult.setText(str(resp))
             except:
                 self.ui.teSteps.clear()
                 self.ui.teSteps.setText("Ingrese los datos corectamente")
-              
+                
+                
+        def pushbutton_ClickedNewton(self):
+            lista=[]
+            #Newton("cos(x)-x",1,0.00005,50,lista)
+            #test=bis("2*(x**3)-4*(x**2)+4*(x)+4",-1,-2,0.00005,50)
+            try:
                
+                resp=Newt(str(self.ui.leEquation.text()),int(str(self.ui.leParam1.text())),float(str(self.ui.leParam2.text())),int(str(self.ui.leParam3.text())),lista)
+                for n in lista:
+                    self.ui.teSteps.append(n)
+        
+                self.ui.lbResult.setText(str(resp))
+            except:
+                self.ui.teSteps.clear()
+                self.ui.teSteps.setText("Ingrese los datos corectamente")
+              
+        
+      
             
             
             
         def checkIndex(self):
             index = self.ui.cboMethod.currentIndex()
             def methodBisection():
+                resetAll(self)
                 self.ui.lbParam1.setText("Limite inferior A")
                 self.ui.lbParam2.setText("Limite superior B")
                 self.ui.lbParam3.setText("Tolerancia")
@@ -92,8 +75,12 @@ class Main(QtGui.QMainWindow):
                 self.ui.lbParam2.show()
                 self.ui.lbParam3.show()
                 self.ui.lbParam4.show()
+                self.ui.lbParam5.hide()
+                self.ui.leParam5.hide()
+                self.ui.pbCalculate.clicked.connect(self.pushbutton_ClickedBiseccion)
                 
             def methodPuntoFijo():
+                resetAll(self)
                 self.ui.lbParam1.setText("Ecuacion Secundaria")
                 self.ui.lbParam2.setText("Aproximacion Inicial")
                 self.ui.lbParam3.setText("Tolerancia")
@@ -106,8 +93,12 @@ class Main(QtGui.QMainWindow):
                 self.ui.lbParam2.show()
                 self.ui.lbParam3.show()
                 self.ui.lbParam4.show()
+                self.ui.lbParam5.hide()
+                self.ui.leParam5.hide()
+               
                 
             def methodNewton():
+                resetAll(self)
                 self.ui.lbParam1.setText("Aproximacion Inicial")
                 self.ui.lbParam2.setText("Tolerancia")
                 self.ui.lbParam3.setText("No. Iteraciones")
@@ -117,8 +108,15 @@ class Main(QtGui.QMainWindow):
                 self.ui.lbParam1.show()
                 self.ui.lbParam2.show()
                 self.ui.lbParam3.show()
+                self.ui.lbParam4.hide()
+                self.ui.leParam4.hide()
+                self.ui.lbParam5.hide()
+                self.ui.leParam5.hide()
+                self.ui.pbCalculate.clicked.connect(self.pushbutton_ClickedNewton)
+               
                 
             def methodSecanteAndPosicionFalsa():
+                resetAll(self)
                 self.ui.lbParam1.setText("Aproximacion Inicial P0")
                 self.ui.lbParam2.setText("Aproximacion Inicial P1")
                 self.ui.lbParam3.setText("Tolerancia")
@@ -131,8 +129,11 @@ class Main(QtGui.QMainWindow):
                 self.ui.lbParam2.show()
                 self.ui.lbParam3.show()
                 self.ui.lbParam4.show()
+                self.ui.lbParam5.hide()
+                self.ui.leParam5.hide()
                 
             def methodMuller():
+                resetAll(self)
                 self.ui.lbParam1.setText("Aproximacion Inicial 1")
                 self.ui.lbParam2.setText("Aproximacion Inicial 2")
                 self.ui.lbParam3.setText("Aproximacion Inicial 3")
@@ -159,30 +160,10 @@ class Main(QtGui.QMainWindow):
                 5 : methodMuller
             }
             options[index]()
-          
             
-            # def resetAll(self):
-            
-            
-        def cleanHistory(self):
-            self.ui.teSteps("")
+            return index
         
-
-            
-            
-        def test(self):
-            
-            '''
-            resp=bis(str(self.ui.lineEdit.text()),int(str(self.ui.lineEdit_2.text())),int(str(self.ui.lineEdit_3.text())),float(str(self.ui.lineEdit_4.text())),int(str(self.ui.lineEdit_5.text())),lista)
-            for n in lista:
-                self.ui.textEditMessage.append(n)
-            self.ui.lineEdit_6Repuesta.setText(str(resp))
-            '''
-            lista = []
-            res = bis("x**2 +4*(x**2)-10",1,2,0.00005,50, lista)
-            
-            print(res)
-        
+              
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)

ac620e20e5fb17da1185f893b6ae149eec04d198
Marco Hernandez <marcohr@unitec.edu>
2014-05-14 15:55:49 -0600

    Fixed Muller

--- ProyectoMetodosNumericos/muller.py
+++ ProyectoMetodosNumericos/muller.py
@@ -26,7 +26,7 @@ def muller(ecuacion, x0,x1,x2,tol,N,lista):
     lista.append('s1 = (f(x1)-f(x0))/h1; s1 = ' + str(s1))
     s2 = (f(x2)-f(x1))/h2
     lista.append('s2 = (f(x2)-f(x1))/h2; s2 = ' + str(s2))
-    d = (s2 - s1)/(h2 - h1)
+    d = (s2 - s1)/(h2 + h1)
     lista.append('d = (s2 - s1)/(h2 - h1); d = ' + str(d))
     i = 3
     lista.append('i = 3')
@@ -69,7 +69,7 @@ def muller(ecuacion, x0,x1,x2,tol,N,lista):
         lista.append('s1 = (f(x1)-f(x0))/h1; s1 = ' + str(s1))
         s2 = (f(x2)-f(x1))/h2
         lista.append('s2 = (f(x2)-f(x1))/h2; s2 = ' + str(s2))
-        d = (s2 - s1)/(h2 - h1)
+        d = (s2 - s1)/(h2 + h1)
         lista.append('d = (s2 - s1)/(h2 - h1); d = ' + str(d))
         i += 1
         lista.append('Incrementar i; i = ' + str(i))

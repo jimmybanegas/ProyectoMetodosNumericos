e314431e08bd65e8856c4e0b99151671ae99e78e
jimmybanegas93 <jimmy.ramos@unitec.edu>
2014-11-02 17:48:07 -0600

    BAT RESOURCES

--- ProyectoMetodosNumericos/resources.bat
+++ ProyectoMetodosNumericos/resources.bat
@@ -1,3 +1,3 @@
-C:\WinPython-64bit-2.7.6.4\python-2.7.6.amd64\Lib\site-packages\PyQt4>pyrcc4 -py
-3 C:\Users\Auditor3\Documents\ProyectoMetodosNumericos\ProyectoMetodosNumericos\
-lylibrary\ui\images.qrc -o images_rc.py
+C:\Users\Auditor3\Documents\ProyectoMetodosNumericos\ProyectoMetodosNumericos\Ve
+ntanas\imagenes> C:\WinPython-64bit-2.7.6.4\python-2.7.6.amd64\Lib\site-packages
+\PyQt4\pyrcc4 imagenes.qrc -o imagenes_rc.py

019fac22aa6afdcc909f713b7d117d8d48d49169
franklin2 <franklin_m2987@hotmail.com>
2014-11-13 20:34:49 -0600

    Actualizacion de la ventana loging con parametros de envio y la pantalla input

--- ProyectoMetodosNumericos/FuncionesDeVentanas/Login.py
+++ ProyectoMetodosNumericos/FuncionesDeVentanas/Login.py
@@ -52,6 +52,7 @@ class IngresarFuncion(QtGui.QMainWindow,Ui_MainWindow):
             self.ui.btnContinuar.clicked.connect(self.Continuar)
             self.ui.btnEvaluar.clicked.connect(self.Evaluar)
             global colorFondo
+            global metodoSeleccionado
             self.setStyleSheet("background-color: "+colorFondo);
     
     #Limpiar los datos del espacio para ingresar funciones
@@ -78,11 +79,63 @@ class ElegirAlgoritmo(QtGui.QMainWindow,Ui_MainWindow):
             self.ui.pbAlgoritmo.clicked.connect(self.EjecutarAlgoritmo)
             self.ui.pbHistorial.clicked.connect(self.VerHistorial)
             global colorFondo
+            global metodoSeleccionado
             self.setStyleSheet("background-color: "+colorFondo);
             
         def EjecutarAlgoritmo(self): 
+            self.SeleccionarMetodo() 
             self.w2 = Input()
-            self.w2.show()     
+            self.w2.show()
+            
+        def SeleccionarMetodo(self):
+            global metodoSeleccionado
+            if self.ui.chBiseccion.isChecked():
+                metodoSeleccionado = "Biseccion"
+            elif self.ui.chNewton.isChecked():
+                metodoSeleccionado = "Newton" 
+            elif self.ui.chSecante.isChecked():
+                metodoSeleccionado = "Secante"
+            elif self.ui.chFalsa.isChecked():
+                metodoSeleccionado = "Falsa"
+            elif self.ui.chMuller.isChecked():
+                metodoSeleccionado = "Muller"
+            elif self.ui.chLagrage.isChecked():
+                metodoSeleccionado = "Lagrage" 
+            elif self.ui.chPolinomialNewton.isChecked():
+                metodoSeleccionado = "PolinomialNewton"
+            elif self.ui.chCubicosNaturales.isChecked():
+                metodoSeleccionado = "CubitosNaturales"
+            elif self.ui.chCubicosSujetos.isChecked():
+                metodoSeleccionado = "CubitosSujetos"
+            elif self.ui.chPuntoFijo.isChecked():
+                metodoSeleccionado = "PuntoFijo"
+            elif self.ui.chDiferenciacion.isChecked():
+                metodoSeleccionado = "Diferenciacion"
+            elif self.ui.chInTrapecio.isChecked():
+                metodoSeleccionado = "InTrapecio"
+            elif self.ui.chInSimpson.isChecked():
+                metodoSeleccionado = "InSimpson"
+            elif self.ui.chInGauss.isChecked():
+                metodoSeleccionado = "InGauss"
+            elif self.ui.chSolucionEuler.isChecked():
+                metodoSeleccionado = "SolucionEuler"
+            elif self.ui.checkBox.isChecked():
+                metodoSeleccionado = "SolucionRunge"
+            elif self.ui.chSistemasRunge.isChecked():
+                metodoSeleccionado = "SistemaRunge"
+            elif self.ui.chEliGauss.isChecked():
+                metodoSeleccionado = "EliGauss"
+            elif self.ui.chEliGaussJordan.isChecked():
+                metodoSeleccionado = "EliGaussJordan"
+            elif self.ui.chInversa.isChecked():
+                metodoSeleccionado = "Inversa"
+            elif self.ui.chDescomposicion.isChecked():
+                metodoSeleccionado = "Descompasicion"
+            elif self.ui.chRegresion.isChecked():
+                metodoSeleccionado = "Regresion"
+            elif self.ui.chDiferencias.isChecked():
+                metodoSeleccionado = "Diferencias"                            
+     
 
         def VerHistorial(self): 
             self.w2 = Historial()
@@ -133,7 +186,136 @@ class Input(QtGui.QMainWindow,Ui_MainWindow):
             self.ui.setupUi(self)
             self.ui.pbCalculate.clicked.connect(self.Calcular)
             global colorFondo
+            global metodoSeleccionado
             self.setStyleSheet("background-color: "+colorFondo);
+            if metodoSeleccionado == "Biseccion":
+                self.Biseccion()                                          
+            elif metodoSeleccionado == "Newton":
+                self.Newton()                 
+            elif metodoSeleccionado == "Secante":
+                self.Secante()
+            elif metodoSeleccionado == "Falsa":
+                self.Falsa()
+            elif metodoSeleccionado == "Muller":
+                self.Muller()
+            elif metodoSeleccionado == "Lagrage":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />") 
+            elif metodoSeleccionado == "PolinomialNewton":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "CubitosNaturales":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "CubitosSujetos":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "PuntoFijo":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "Diferenciacion":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "InTrapecio":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "InSimpson":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "InGauss":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "SolucionEuler":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "SolucionRunge":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "SistemaRunge":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "EliGauss":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "EliGaussJordan":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "Inversa":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "Descompasicion":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "Regresion":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            elif metodoSeleccionado == "Diferencias":
+                self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />") 
+                
+#self.ui.lbEjemplo.setPixmap(QPixmap("Ventanas\imagenes\Biseccion.JPG"))      
+        
+        def Biseccion(self):
+            self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            self.ui.lbParam1.setText("Limite inferior A")
+            self.ui.lbParam2.setText("Limite superior B")
+            self.ui.lbParam3.setText("Tolerancia")
+            self.ui.lbParam4.setText("No. Iteraciones")
+            self.ui.leParam1.show()
+            self.ui.leParam2.show()
+            self.ui.leParam3.show()
+            self.ui.leParam4.show()
+            self.ui.lbParam1.show()
+            self.ui.lbParam2.show()
+            self.ui.lbParam3.show()
+            self.ui.lbParam4.show()
+            self.ui.lbParam5.hide()
+            self.ui.leParam5.hide() 
+        def Newton(self):
+            self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            self.ui.lbParam1.setText("Aproximacion Inicial")
+            self.ui.lbParam2.setText("Tolerancia")
+            self.ui.lbParam3.setText("No. Iteraciones")
+            self.ui.leParam1.show()
+            self.ui.leParam2.show()
+            self.ui.leParam3.show()
+            self.ui.lbParam1.show()
+            self.ui.lbParam2.show()
+            self.ui.lbParam3.show()
+            self.ui.lbParam4.hide()
+            self.ui.leParam4.hide()
+            self.ui.lbParam5.hide()
+            self.ui.leParam5.hide()
+        def Secante(self):
+            self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            self.ui.lbParam1.setText("Aproximacion Inicial P0")
+            self.ui.lbParam2.setText("Aproximacion Inicial P1")
+            self.ui.lbParam3.setText("Tolerancia")
+            self.ui.lbParam4.setText("No. Iteraciones")
+            self.ui.leParam1.show()
+            self.ui.leParam2.show()
+            self.ui.leParam3.show()
+            self.ui.leParam4.show()
+            self.ui.lbParam1.show()
+            self.ui.lbParam2.show()
+            self.ui.lbParam3.show()
+            self.ui.lbParam4.show()
+            self.ui.lbParam5.hide()
+            self.ui.leParam5.hide()
+        def Falsa(self):
+            self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            self.ui.lbParam1.setText("Aproximacion Inicial P0")
+            self.ui.lbParam2.setText("Aproximacion Inicial P1")
+            self.ui.lbParam3.setText("Tolerancia")
+            self.ui.lbParam4.setText("No. Iteraciones")
+            self.ui.leParam1.show()
+            self.ui.leParam2.show()
+            self.ui.leParam3.show()
+            self.ui.leParam4.show()
+            self.ui.lbParam1.show()
+            self.ui.lbParam2.show()
+            self.ui.lbParam3.show()
+            self.ui.lbParam4.show()
+            self.ui.lbParam5.hide()
+            self.ui.leParam5.hide()
+        def Muller(self):
+            self.ui.lbEjemplo.setText("<img src=..\Ventanas\imagenes\Ejemplos\system-users.png />")
+            self.ui.lbParam1.setText("Aproximacion Inicial P0")
+            self.ui.lbParam2.setText("Aproximacion Inicial P1")
+            self.ui.lbParam3.setText("Tolerancia")
+            self.ui.lbParam4.setText("No. Iteraciones")
+            self.ui.leParam1.show()
+            self.ui.leParam2.show()
+            self.ui.leParam3.show()
+            self.ui.leParam4.show()
+            self.ui.lbParam1.show()
+            self.ui.lbParam2.show()
+            self.ui.lbParam3.show()
+            self.ui.lbParam4.show()
+            self.ui.lbParam5.hide()
+            self.ui.leParam5.hide()
             
         def Calcular(self): 
             self.w2 = Graph()

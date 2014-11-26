e4e93dc650074a626fc8c48c8618be77bc6f0097
CarlosEscobar <cai_escobar@hotmail.com>
2014-11-26 13:09:16 -0600

    AlgoritmoRegresionLineal

--- ProyectoMetodosNumericos/Algoritmos/Reduccion_Matrices_Gauss.py
+++ ProyectoMetodosNumericos/Algoritmos/Reduccion_Matrices_Gauss.py
@@ -3,56 +3,62 @@ Created on Nov 16, 2014
 
 @author: carlosa
 '''
-import math
-from math import *
 
-def IngresarMatriz ():
-    
+def IngresarValores(matrizV, numfil):
+        s = 0
+        while s < numfil:
+            sp = s + 1
+            sri = "Ingrese la fila " + str(sp)
+            fila = map(int, raw_input(sri).split())
+            matrizV.append(fila)
+            s = s + 1
+            
 
-    if __name__ == '__main__':
- 
-        print "SISTEMAS DE ECUACIONES POR ELIMINACION DE GAUSS"
-        n = int(raw_input('Ingrese el numero de filas de la matriz a reducir: '))
-        m = int(raw_input('Ingrese el numero de columnas de la matriz a reducir: '))
- 
-    s = 0
-    matrizA
-    while s < n:
-        sp = s + 1
-        sri = "Ingrese la fila " + str(sp)
-        fila = map(int, raw_input(sri).split())
-        matrizA.append(fila)
-        s = s + 1
+def Reduccion_Gauss(matrizV, numfil, numcol):
+        i = 0
+        while i < numcol:
+            if matrizV[i][i] == 0:
+                j=i
+                while j < numcol:
+                    if matrizV[j][i] != 0:
+                        temp = matrizV[j]
+                        matrizV[j] = matrizV[i]
+                        matrizV[i] = temp
+                        j = numcol
+                    elif ((matrizV[j][i] == 0) and (j==numcol)):
+                        print "La matriz no se puede reducir"
+                    j = j + 1
+            if matrizV[i][i] != 1:
+                z = i
+                while z < numfil:
+                    matrizV[i][z] = matrizV[i][z]/matrizV[i][i]
+                    x = 0
+                    while x < numcol:
+                        w = 0
+                        while w < numfil:
+                            if x != i:
+                                if x != 0:
+                                    factor = (-1)*(matrizV[x][w])
+                                matrizV[x][w] = matrizV[x][w] + matrizV[x][w]*factor
+                            w = w + 1
+                        x = x + 1
+                    z = z + 1
+            i = i + 1
+        print "Proceso realizado exitosamente"
+        f = 0
+        while f < numfil:
+            g = 0
+            while g < numcol:
+                print matrizV[f][g]
+                g = g + 1
+            f = f + 1
+
+if __name__ == '__main__':
+    print "SISTEMAS DE ECUACIONES POR ELIMINACION DE GAUSS"
+    n = int(raw_input('Ingrese el numero de filas de la matriz a reducir: '))
+    m = int(raw_input('Ingrese el numero de columnas de la matriz a reducir: '))
+    matrizA = []
+    IngresarValores(matrizA,n)
     print "Reduciendo Matriz"
-    i = 0
-    while i < m:
-        if matrizA([i,i]) == 0:
-            j=i
-            while j < m:
-                if matrizA([j,i]) != 0:
-                    temp = matrizA[j]
-                    matrizA[j] = matrizA[i]
-                    matrizA[i] = temp
-                    j = m
-                elif ((matrizA([j,i]) == 0) & (j=m)):
-                    print "La matriz no se puede reducir"
-                j = j + 1
-        if matrizA([i,i] != 1):
-            z = i
-            while z < n:
-                matrizA([i,z]) = (matrizA([i,z])/matrizA([i,i]))
-                x = 0
-                while x < m:
-                    w = 0
-                    while w < n:
-                        if x != i:
-                            if x != 0:
-                                factor = (-1)*(matrizA([x,w]))
-                            matrizA([x,w]) = matrizA([x,w]) + matrizA([x,w])*factor
-                        w = w + 1
-                    x = x + 1
-                z = z + 1
-        i = i + 1
-    print "Proceso realizado exitosamente"    
-                
- raw_input("Presione una tecla para finalizar")
+    Reduccion_Gauss(matrizA, n, m)
+    raw_input("Presione una tecla para finalizar")
--- ProyectoMetodosNumericos/Algoritmos/RegresionLineal.py
+++ ProyectoMetodosNumericos/Algoritmos/RegresionLineal.py
@@ -4,35 +4,30 @@ Created on Nov 16, 2014
 @author: carlosa
 '''
 
-import math
-from math import *
-from test.test_support import temp_cwd
-
+def RegresionLineal(valoresX, valoresY, Xo):
+        m = len(valoresX)
+        constante1 = 0
+        constante2 = 0
+        constante3 = 0
+        constante4 = 0
+        i = 0
+        while i < m:
+            constante1 = constante1 + valoresY[i]
+            constante2 = constante2 + (valoresX[i] * valoresX[i])
+            constante3 = constante3 + valoresX[i]
+            constante4 = constante4 + (valoresX[i] * valoresY[i])
+            i = i + 1
+        coeficiente1 = ((constante1*constante2) - (constante3*constante4))/((m * constante2)-(constante3*constante3))
+        coeficiente2 = ((m*constante4) - (constante1*constante3))/((m * constante2)-(constante3*constante3))
+        return coeficiente1 + (Xo*coeficiente2)
 
 
 if __name__ == '__main__':
     print "Metodo de Regresion Lineal"
-    n = int(raw_input('Ingrese el numero de datos: '))
-    lx = map(int, raw_input("Ingrese los valores de x: ").split())
-    ly = map(int, raw_input("Ingrese los valores de y: ").split())
+    lx = map(float, raw_input("Ingrese los valores de x: ").split())
+    ly = map(float, raw_input("Ingrese los valores de y: ").split())
+    x = raw_input("Ingrese el valor de la x a aproximar: ")
+    xx = float(x)
+    y = RegresionLineal(lx,ly,xx)
     
-    sumx = 0
-    sumy = 0
-    sumxy = 0
-    sumx2 = 0
-    i = 0
-    
-    while i < n:
-        sumx = sumx + lx[i]
-        sumy = sumy + ly[i]
-        sumx2 = sumx2 + (lx[i]*lx[i])
-        sumxy = sumxy + (lx[i]*ly[i])
-        i = i + 1
-        
-    denominador = (sumx*sumy) - (n*sumx2)
-    
-    m = (sumx*sumy - n*sumxy)/denominador
-    b = (sumx*sumxy - sumx2*sumy)/denominador
-    
-    print "m: " + str(m) + " , b: " + str(b)
-    
+    print "y=" + str(y) + " para x=" + x  

b0cac91b0b9ca5daa00ae1371eba4e00049f40ae
jimmybanegas93 <jimmy.ramos@unitec.edu>
2014-11-26 18:36:19 -0600

    Confl

--- ProyectoMetodosNumericos/Ventanas/Historial.py
+++ ProyectoMetodosNumericos/Ventanas/Historial.py
@@ -1,69 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Historial.ui'
-#
-# Created: Mon Nov 17 20:50:16 2014
-#      by: PyQt4 UI code generator 4.9.6
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(415, 561)
-        self.centralwidget = QtGui.QWidget(MainWindow)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.cbfx = QtGui.QComboBox(self.centralwidget)
-        self.cbfx.setGeometry(QtCore.QRect(160, 30, 201, 41))
-        self.cbfx.setObjectName(_fromUtf8("cbfx"))
-        self.teSteps = QtGui.QTextEdit(self.centralwidget)
-        self.teSteps.setGeometry(QtCore.QRect(30, 100, 361, 401))
-        self.teSteps.setObjectName(_fromUtf8("teSteps"))
-        self.lbfx = QtGui.QLabel(self.centralwidget)
-        self.lbfx.setGeometry(QtCore.QRect(40, 30, 71, 31))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.lbfx.setFont(font)
-        self.lbfx.setObjectName(_fromUtf8("lbfx"))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 415, 21))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.lbfx.setText(_translate("MainWindow", "f(x) =", None))
-
-<<<<<<< HEAD
-=======
-import imagenes_rc
-# -*- coding: utf-8 -*-
-
 # Form implementation generated from reading ui file 'C:\Users\manuel\Documents\ProyectoMetodos\ProyectoMetodosNumericos\ProyectoMetodosNumericos\Ventanas\Historial.ui'
 #
 # Created: Tue Nov 25 19:29:10 2014
@@ -173,4 +109,4 @@ class Ui_mainWindow(object):
         self.btnRegresar.setText(_translate("mainWindow", "Regresar", None))
 
 import imagenes_rc
->>>>>>> master
+

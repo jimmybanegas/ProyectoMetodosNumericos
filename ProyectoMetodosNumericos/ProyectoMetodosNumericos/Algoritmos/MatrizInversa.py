b2d4d9c60973b65fdffce4b5ed445d987ea18d4c
jimmybanegas93 <jimmy.ramos@unitec.edu>
2014-12-09 19:13:56 -0600

    ms

--- ProyectoMetodosNumericos/ProyectoMetodosNumericos/Algoritmos/MatrizInversa.py
+++
@@ -1,35 +0,0 @@
-#coding=utf-8
-from sympy import Matrix
-import json
-def MatrizInversa(a, b,lista):    
-    ma = json.loads(a)    
-    mb = json.loads(b)    
-    A = Matrix(ma)    
-    B = Matrix(mb)    
-    try:    
-        A_inv = A.inv()    
-    except ValueError:    
-        lista.append('\n')    
-        lista.append( 'Matriz es singular, no se puede resolver!')    
-        lista.append('\n')     
-    else:    
-        ans = A_inv*B    
-        MA = A.tolist()    
-        MB = B.tolist()    
-        MA_inv = A_inv.tolist()    
-        M_ans = ans.tolist()    
-        lista.append(' Matriz A = ')    
-        lista.append('\t|   '+('|\n\t|   '.join([''.join(['{:4}'.format(item) for item in row]) for row in MA]))+'|')    
-        lista.append('\n')    
-        lista.append( 'Vector B = ')    
-        lista.append( '\t|   '+('|\n\t|   '.join([''.join(['{:4}'.format(item) for item in row]) for row in MB]))+'|')    
-        lista.append('\n')    
-        lista.append( 'Matriz Inversa de A^-1 =')    
-        lista.append('\t|   '+('\t|\n\t|   '.join(['\t'.join(['{:4}'.format(item) for item in row]) for row in MA_inv]))+'\t|')    
-        lista.append('\n')    
-        lista.append( 'Respuesta A^-1 * B =')    
-        lista.append('\t|   '+('|\n\t|   '.join([''.join(['{:4}'.format(item) for item in row]) for row in M_ans]))+'|')
-        return 0        
-
-
-
--- ProyectoMetodosNumericos/ProyectoMetodosNumericos/resources.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\Auditor3\Documents\ProyectoMetodosNumericos\ProyectoMetodosNumericos\Ve
-ntanas\imagenes> C:\WinPython-64bit-2.7.6.4\python-2.7.6.amd64\Lib\site-packages
-\PyQt4\pyrcc4 imagenes.qrc -o imagenes_rc.py
